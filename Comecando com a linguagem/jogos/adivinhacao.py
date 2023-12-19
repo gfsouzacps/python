ee96b98c35481a42e66118821800eae7e428a70c
gfsouzacps <gui240799@outlook.com>
2023-12-18 22:43:37 -0300

    Interpolação, laço while, conversão, if e elif

---
+++ Comecando com a linguagem/jogos/adivinhacao.py
@@ -0,0 +1,32 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+total_tentativas = 3
+rodada = 1
+
+while rodada <= total_tentativas:
+    print(f"Tentativa {rodada} de {total_tentativas}")
+    chute = input("Digite o seu numero: ")
+
+    # Chute nesse caso recebe uma str. Devido a isso, ao fazer o if temos false independente do número.
+    # Para isso fizemos int(chute) que faz a conversão
+    conv_chute = int(chute)
+    acertou = numero_secreto == conv_chute
+    maior = conv_chute > numero_secreto
+    menor = conv_chute < numero_secreto
+
+    if acertou:
+        print("Voce acertou!")
+        break
+    else:
+        if maior:
+            print("Você errou, o chute foi maior que o numero secreto!")
+        elif menor:
+            print("Voce errou, o chute foi menor que o numero secreto!")
+
+    rodada = rodada + 1
+
+    print("Fim do jogo")
+    print("*****************************")
